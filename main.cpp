6a108acb47ce6d5cf99c898638a3be662a59b654
Drool3008 <dhawalpawanarkar123@gmail.com>
2023-10-16 11:15:43 +0530

    Updated files

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Insert_in_Trie.h"
+#include "Search_In_Trie.h"
 int main() {
     vector<string> dict;
     dict.emplace_back("ARE");
@@ -6,10 +6,16 @@ int main() {
     dict.emplace_back("YOU");
     dict.emplace_back("YOUR");
     dict.emplace_back("THE");
+    dict.emplace_back("DO");
+    dict.emplace_back("DOT");
+    dict.emplace_back("NEW");
+    dict.emplace_back("NEWS");
     auto root = new Node();
     for(const auto & i : dict)
     {
         add(i,root);
     }
+    if(is_Present(root,"DOTO")) cout<<"Present";
+    else cout<<"Not Present";
     return 0;
 }
